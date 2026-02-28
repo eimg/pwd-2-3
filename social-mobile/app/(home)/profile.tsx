704e8699c5da19307877b4e83146c73336a26a49
Ei Maung <eimg@fairwayweb.com>
2026-02-28 13:30:06 +0630

    mobile login

--- social-mobile/app/(home)/profile.tsx
+++ social-mobile/app/(home)/profile.tsx
@@ -1,12 +1,121 @@
-import { Text, View } from "react-native";
+import { Text, View, Button, TextInput, Alert } from "react-native";
+import { useState } from "react";
+import { useApp } from "@/components/AppProvider";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
+
+const api = "http://localhost:8800/users/login";
 
 export default function Profile() {
-    return (
-        <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text style={{ fontWeight: "bold", fontSize: 21 }}>
-                Profile
-            </Text>
-        </View>
-    );
+	const { user, setUser } = useApp();
+
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const login = async () => {
+		const res = await fetch(api, {
+			method: "POST",
+			body: JSON.stringify({ username, password }),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+		if (res.ok) {
+			const { token, user } = await res.json();
+			setUser(user);
+			await AsyncStorage.setItem("token", token);
+			router.push("/");
+		} else {
+			Alert.alert("Error", "Invalid username or password");
+		}
+	};
+
+	return (
+		<View
+			style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			{user ? (
+				<View>
+					<View
+						style={{
+							marginBottom: 20,
+							alignItems: "center",
+							justifyContent: "center",
+						}}>
+						<Text
+							style={{
+								fontWeight: "bold",
+								fontSize: 21,
+								marginBottom: 16,
+							}}>
+							Profile
+						</Text>
+						<Text style={{ fontSize: 16, fontWeight: "bold" }}>
+							{user.name}
+						</Text>
+						<Text style={{ color: "#999999" }}>
+							{user.username}
+						</Text>
+					</View>
+					<Button
+						title="Logout"
+						onPress={() => setUser(null)}
+					/>
+				</View>
+			) : (
+				<View
+					style={{
+						alignItems: "center",
+						width: "100%",
+						flex: 1,
+						justifyContent: "center",
+					}}>
+					<Text
+						style={{
+							fontWeight: "bold",
+							fontSize: 21,
+							marginBottom: 20,
+						}}>
+						Login
+					</Text>
+					<TextInput
+						autoCapitalize="none"
+						placeholder="Username"
+						value={username}
+						onChangeText={setUsername}
+						style={{
+							borderWidth: 1,
+							borderColor: "#99999980",
+							borderRadius: 20,
+							padding: 10,
+							marginBottom: 10,
+							fontSize: 18,
+							width: "80%",
+							alignSelf: "center",
+						}}
+					/>
+					<TextInput
+						secureTextEntry={true}
+						placeholder="Password"
+						value={password}
+						onChangeText={setPassword}
+						style={{
+							borderWidth: 1,
+							borderColor: "#99999980",
+							borderRadius: 20,
+							padding: 10,
+							marginBottom: 10,
+							fontSize: 18,
+							width: "80%",
+							alignSelf: "center",
+						}}
+					/>
+					<Button
+						title="Login"
+						onPress={login}
+					/>
+				</View>
+			)}
+		</View>
+	);
 }
--- social-mobile/app/_layout.tsx
+++ social-mobile/app/_layout.tsx
@@ -1,25 +1,29 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import AppProvider from "@/components/AppProvider";
+
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Stack>
-				<Stack.Screen
-					name="(home)"
-					options={{
-						title: "Home",
-					}}
-				/>
-				<Stack.Screen
-					name="view/[id]"
-					options={{
-						title: "View",
-					}}
-				/>
-			</Stack>
+			<AppProvider>
+				<Stack>
+					<Stack.Screen
+						name="(home)"
+						options={{
+							title: "Home",
+						}}
+					/>
+					<Stack.Screen
+						name="view/[id]"
+						options={{
+							title: "View",
+						}}
+					/>
+				</Stack>
+			</AppProvider>
 		</QueryClientProvider>
 	);
 }
